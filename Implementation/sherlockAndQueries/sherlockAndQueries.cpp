1c1e8352bae6102f4e84c8ebaec468e6b3905958
Joseph Lin <joseph.lin.163@gmail.com>
2015-02-08 22:44:53 -0800

    Added implementation details for sherlockAndQueries.

--- Implementation/sherlockAndQueries/sherlockAndQueries.cpp
+++ Implementation/sherlockAndQueries/sherlockAndQueries.cpp
@@ -11,6 +11,19 @@ int main() {
 
     // Clearly, this code is wrong, I will revise it as soon as I finish studying
     // for my classes.
+
+    /*
+        Implementation goes along the lines of:
+        for (int i = 0; i < M; i++) {
+            for (int j = B[i]; j < N; j+= B[i]) {
+                A[j] *= C[i];
+            }
+        }
+
+        The idea is that we have a single computation per term,
+        A = a1 * a2 * a3 * ... * an
+        Where A is the product of the terms a1 through an.
+    */
     int N, M;
     int mod = pow(10,9) + 7;
     cin >> N >> M;
